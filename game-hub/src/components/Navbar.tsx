615986c39dc4e39376b59df060cc8aa61dda2cb4
vitalrr <114648945+Abomba06@users.noreply.github.com>
2025-01-03 19:12:20 +0300

    Added game card skeletons

--- game-hub/src/components/Navbar.tsx
+++ game-hub/src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/logo.webp";
+import logo from "../assets/pfp.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const Navbar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
-      <Image src={logo} boxSize="60px" />
+      <Image src={logo} boxSize="60px" borderRadius={15} />
       <ColorModeSwitch />
     </HStack>
   );
