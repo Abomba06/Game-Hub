c801581c9d59eb37424307672048ae6e929d71f4
vitalrr <114648945+Abomba06@users.noreply.github.com>
2025-01-04 13:05:15 +0300

    Displayed Genres

--- game-hub/src/components/GenresList.tsx
+++ game-hub/src/components/GenresList.tsx
@@ -1,14 +1,25 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageURL from "../services/image-url";
 
 const GenresList = () => {
   const { data } = useGenres();
 
   return (
-    <ul>
-      {data?.map((g) => (
-        <li key={g.id}>{g.name}</li>
+    <List>
+      {data?.map((d) => (
+        <ListItem key={d.id} paddingY={1}>
+          <HStack>
+            <Image
+              src={getCroppedImageURL(d.image_background)}
+              boxSize={"32px"}
+              borderRadius={8}
+            />
+            <Text fontSize="lg">{d.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
